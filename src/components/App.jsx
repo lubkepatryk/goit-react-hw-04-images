aee4f642534c83902f7aeb3a05832360208e032e
lubkepatryk <lubke.patryk@gmail.com>
2023-06-28 21:34:25 +0200

    updated app jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,27 @@ const App = () => {
   const [modalImageURL, setModalImageURL] = useState('');
 
   useEffect(() => {
+    const fetchImages = async () => {
+      if (searchQuery === '') return;
+
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          `https://pixabay.com/api/?q=${encodeURIComponent(
+            searchQuery
+          )}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        const data = await response.json();
+
+        setImages(prevImages => [...prevImages, ...data.hits]);
+      } catch (error) {
+        console.log('Error fetching images:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchImages();
   }, [searchQuery, page]);
 
@@ -38,27 +59,6 @@ const App = () => {
   const handleCloseModal = () => {
     setShowModal(false);
     setModalImageURL('');
-  };
-
-  const fetchImages = async () => {
-    if (searchQuery === '') return;
-
-    setIsLoading(true);
-
-    try {
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${encodeURIComponent(
-          searchQuery
-        )}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      const data = await response.json();
-
-      setImages(prevImages => [...prevImages, ...data.hits]);
-    } catch (error) {
-      console.log('Error fetching images:', error);
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   return (
